caf45f7d8b6b6947ebfc8ffe42d4de74294f3acc
LeoOrVyus <clasescursosymas@gmail.com>
2024-07-20 12:18:44 -0600

    postgress

---
+++ todo-app/src/main/java/com/todoalura/todo_app/entity/Task.java
@@ -0,0 +1,31 @@
+package com.todoalura.todo_app.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private boolean completed;
+
+    private LocalDate dueDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
